4a7a82cb1d98945714940a9b1ef47f4ca047f826
Priscilla <comptepro@mbpdepriscilla.home>
2023-02-15 16:12:22 +0100

    create component Header

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../img/Marvel_Logo.svg.png";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="Header">
+        <div className="Logo">
+          <Link to="/">
+            <img src={logo} alt="Logo Marvel" />
+          </Link>
+        </div>
+        <nav className="Nav">
+        <button><Link to="/characters">CHARACTERS</Link></button>
+        <button><Link to="/comics">COMICS</Link></button>
+        <button><Link to="/favs">MY FAVS</Link></button>
+        </nav>
+        <div className="Login">
+          <button className="Logbutton">
+            Login
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
