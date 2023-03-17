212fe40264e0f8ca861b71b03670235096d54e0e
Priscilla <comptepro@mbpdepriscilla.home>
2023-03-17 18:46:46 +0100

    Update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,33 @@
+import Marvel from "../img/footer-Marvicon-194.png"
+
 const Footer = () => {
     return (
-      <div className="Footer-contain">
+      // <div className="Footer-contain">
        <div className="Footer">
+        <div>
+          <img alt="icon" src={Marvel} />
+        </div>
+       <div className="Block1">
+          <p>ABOUT MARVEL</p>
+          <p>HELP/FAQS</p>
+          <p>CAREERS</p>
+          <p>INTERNSHIPS</p>
+       </div>
+       <div className="Block2">
+          <p>ADVERTISING</p>
+          <p>DISNEY+</p>
+          <p>MARVELHQ.COM</p>
+          <p>REDEEM DIGITAL COMICS</p>
+       </div>
+       <div>
         <p>
           Made with <span>React</span> with <span>Le Reacteur</span> by{" "}
           <span>Priscil</span>{" "}
         </p>
+       </div>
         
       </div> 
-      </div>
+      // {/* </div> */}
       
     );
   };
